d9ead0685971e3e28b659ab781cf5d9d28418212
Jinwhl <103013505+Jinwhl@users.noreply.github.com>
2024-08-27 11:27:22 +0900

    챗봇 Navigation 연결

--- app/src/main/java/com/example/gaenolza/MainScreen.kt
+++ app/src/main/java/com/example/gaenolza/MainScreen.kt
@@ -1,23 +1,41 @@
 package com.example.gaenolza
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
 data class CardItem(val id: Int, val title: String, val content: String)
 
 @Composable
 fun MainScreen(
+    navController: NavController,
     onCardClick: (Int) -> Unit,
     horizontalCardWidth: Dp = 300.dp,
     horizontalCardHeight: Dp = 200.dp,
@@ -102,7 +120,7 @@ fun MainScreen(
         }
 
         FloatingActionButton(
-            onClick = { /* FAB 클릭 시 수행할 작업 */ },
+            onClick = { navController.navigate("chatScreen") },
             shape = CircleShape,
             modifier = Modifier
                 .align(Alignment.BottomEnd)
@@ -152,18 +170,3 @@ fun generateItems(count: Int): List<CardItem> {
         )
     }
 }
-// 메인 액티비티에서 사용 예시
-//class MainActivity : ComponentActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContent {
-//            MaterialTheme {
-//                MainScreen(onCardClick = { cardId ->
-//                    // 카드 클릭 시 처리 로직
-//                    println("Card $cardId clicked")
-//                    // 여기에 네비게이션 로직을 추가할 수 있습니다.
-//                })
-//            }
-//        }
-//    }
-//}
